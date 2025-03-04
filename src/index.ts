effe98c925ca8bc7a2473655fbd2f11aed292adf
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-03-04 19:52:47 +0530

    login route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express, {Express, Request, Response} from 'express';
 import { PORT } from '../secret';
+import rootRouter from './routes';
 
 const app:Express = express();
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('working')
-})
+app.use('/api', rootRouter);
 
 app.listen(PORT, () => {console.log('App working!')})
