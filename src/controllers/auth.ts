ccfcad2a5a6874ddd75e15de1357c58abaea941a
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-03-07 16:01:38 +0530

    Added import of NotFoundException and updated error handling in auth controller for login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,6 +7,7 @@ import { BadRequestsException } from '../exceptions/bad-requests';
 import { ErrorCode } from '../exceptions/root';
 import { UnprocessableEntity } from '../exceptions/validation.';
 import { SignUpSchema } from '../schema/users';
+import { NotFoundException } from '../exceptions/not-found';
  
 export const signup = async (req: Request ,res: Response, next: NextFunction) => {
     SignUpSchema.parse(req.body)
@@ -14,7 +15,7 @@ export const signup = async (req: Request ,res: Response, next: NextFunction) =>
 
     let user = await prismaClient.user.findFirst({where: {email}})
     if (user) {
-        next(new BadRequestsException('User already exists!', ErrorCode.USER_ALREADY_EXISTS))
+        new BadRequestsException('User already exists!', ErrorCode.USER_ALREADY_EXISTS)
     }
     user = await prismaClient.user.create({
         data:{
@@ -31,10 +32,10 @@ export const login = async (req: Request ,res: Response) => {
 
     let user = await prismaClient.user.findFirst({where: {email}})
     if (!user) {
-        throw Error('User does not exists!')
+        throw new NotFoundException('User not found', ErrorCode.USER_NOT_FOUND)
     }
     if(!compareSync(password, user.password)) {
-        throw Error ('Invalid password!')
+        throw new BadRequestsException('Incorrect Password', ErrorCode.INCORRECT_PASSWORD)
     }
     const token = jwt.sign({
         userId: user.id
