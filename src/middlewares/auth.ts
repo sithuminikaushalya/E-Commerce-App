f7820651f9ae4c54badf85041dfbc66ac9e9fcec
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-03-11 20:23:14 +0530

    typescript error solved

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,19 +4,19 @@ import { ErrorCode } from "../exceptions/root";
 import * as jwt from 'jsonwebtoken';
 import { JWT_SECRET } from "../secret";
 import { prismaClient } from "..";
-import { User } from "@prisma/client";
+import { Payload } from "@prisma/client/runtime/library";
 
 const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     // 1. extract the token from header
     const token = req.headers.authorization;
-
+                 
     // 2. if token is not present, throw an error of unauthorized
     if (!token) {
         return next(new UnauthorizedException('Unauthorized', ErrorCode.UNAUTHORIZED));
     }
     try {
         // 3. if the token is present, verify that token and extract the payload
-        const payload = jwt.verify(token, JWT_SECRET) as any;
+        const payload = jwt.verify(token as string, JWT_SECRET) as any;
 
         // 4. to get the user from the payload
         const user = await prismaClient.user.findFirst({where: {id: payload.userId}});
