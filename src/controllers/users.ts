9bc88710448889a73cb0a0f38bb9e316a5f1965e
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-03-14 15:13:14 +0530

    Added NotFoundException and ErrorCode for address not found, updated deleteAddress and listAddress to use Prisma and handle exceptions'

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { AddressSchema } from "../schema/users";
 import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
 
 export const addAddress = async(req: Request, res: Response) => {
     AddressSchema.parse(req.body)
@@ -15,9 +17,23 @@ export const addAddress = async(req: Request, res: Response) => {
 }
 
 export const deleteAddress = async(req: Request, res: Response) => {
-
+    try {
+        await prismaClient.address.delete({
+            where: {
+                id: +req.params.id
+            }
+        })
+        res.json({success: true})
+    } catch(err) {
+        throw new NotFoundException('Address not found',ErrorCode.ADDRESS_NOT_FOUND)
+    }
 }
 
 export const listAddress = async(req: Request, res: Response) => {
-
+    const addAddress = await prismaClient.address.findMany({
+        where: {
+            userId: req.user.id
+        }
+    })
+    res.json(addAddress)
 }
