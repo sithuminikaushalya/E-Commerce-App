f55c7b8ed9ceeb390c5576e01098d52c279d557a
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-03-14 19:12:45 +0530

    create files related to cart and added cart functionality to Prisma schema and Express routes

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const SignUpSchema = z.object ({
     name: z.string(),
-    email: z.string(),
+    email: z.string().email(),
     password: z.string().min(6),
 })
 
