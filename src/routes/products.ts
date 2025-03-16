2521634a66cb213a81e8951d8e5cb86bb3ac1a2e
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-03-16 13:51:58 +0530

    search functionality was done

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { errorHandler } from "../error-handler";
-import { createProduct, deleteProduct, getProductById, listProducts, updateProduct } from "../controllers/products";
+import { createProduct, deleteProduct, getProductById, listProducts, searchProducts, updateProduct } from "../controllers/products";
 import authMiddleware from "../middlewares/auth";
 import adminMiddleware from "../middlewares/admin";
 
@@ -10,6 +10,7 @@ productsRoutes.post('/', [authMiddleware, adminMiddleware] , errorHandler(create
 productsRoutes.put('/:id', [authMiddleware, adminMiddleware], errorHandler(updateProduct));
 productsRoutes.delete('/:id', [authMiddleware, adminMiddleware], errorHandler(deleteProduct));
 productsRoutes.get('/', [authMiddleware, adminMiddleware], errorHandler(listProducts));
+productsRoutes.get('/search', [authMiddleware], errorHandler(searchProducts));
 productsRoutes.get('/:id', [authMiddleware, adminMiddleware], errorHandler(getProductById));
 
 export default productsRoutes;  
