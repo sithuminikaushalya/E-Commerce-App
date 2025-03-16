c30af7b30cbafaf99a2de62214013ab6927bd758
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-03-16 10:33:55 +0530

    updated orders controller to create orders with transactions and formatted addresses

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,7 +1,62 @@
 import { Request, Response } from "express";
+import { prismaClient } from "..";
 
 export const createOrder = async(req: Request, res: Response) => {
-
+    // 1. to create a transaction
+    // 2. to list all the cart items and proceed if cart is not empty
+    // 3. calculate the total amount
+    // 4. fetch address of user
+    // 5. to define computed field for formatted address on address module
+    // 6. we will create a order and order productsorder products
+    // 7. create event
+    // 8. to empty the cart
+    return await prismaClient.$transaction(async(tx) => {
+        const cartItems = await tx.cartItem.findMany({
+            where: {
+                userId: req.user.id
+            },
+            include: {
+                product: true
+            }
+        })
+        if(cartItems.length  == 0) {
+            return res.json({message: "cart is empty"})
+        }
+        const price = cartItems.reduce((prev, current) => {
+            return prev + (current.quantity * +current.product.price)
+        },0);
+        const address = await tx.address.findFirst({
+            where: {
+                id: req.user.defaultShippingAddress
+            }
+        })
+        const order = await tx.order.create({
+            data: {
+                userId: req.user.id,
+                netAmount: price,
+                address: address.formattedAddress,
+                order_products: {
+                    create: cartItems.map((cart) => {
+                        return {
+                            productId: cart.productId,
+                            quantity: cart.quantity
+                        }
+                    })
+                }
+            }
+        })
+        const orderEvent = await tx.orderEvent.create({
+            data: {
+                orderId: order.id,
+            }
+        })
+        await tx.cartItem.deleteMany({
+            where: {
+                userId: req.user.id
+            }
+        })
+        return res.json(order)
+    })    
 }
 
 export const listOrders = async(req: Request, res: Response) => {
@@ -13,5 +68,5 @@ export const cancelOrder = async(req: Request, res: Response) => {
 }
 
 export const getOrderById = async(req: Request, res: Response) => {
-    
+
 }
