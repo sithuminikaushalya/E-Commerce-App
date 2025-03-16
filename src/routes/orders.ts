16203c285b40f474e586ea1b36ec7752de6d675d
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-03-16 13:12:13 +0530

    Added new endpoints for listing all orders, listing user orders, and changing order status

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth";
 import { errorHandler } from "../error-handler";
-import { cancelOrder, createOrder, getOrderById, listOrders } from "../controllers/orders";
+import { cancelOrder, changeStatus, createOrder, getOrderById, listAllorders, listOrders, listUserOrders } from "../controllers/orders";
+import adminMiddleware from "../middlewares/admin";
 
 const orderRoutes:Router = Router()
 
 orderRoutes.post('/', [authMiddleware], errorHandler(createOrder));
 orderRoutes.get('/', [authMiddleware], errorHandler(listOrders));
 orderRoutes.put('/:id/cancel', [authMiddleware], errorHandler(cancelOrder));
+orderRoutes.get('/index', [authMiddleware, adminMiddleware], errorHandler(listAllorders));
+orderRoutes.get('/users/:id', [authMiddleware, adminMiddleware], errorHandler(listUserOrders));
+orderRoutes.put('/:id/status', [authMiddleware, adminMiddleware], errorHandler(changeStatus));
 orderRoutes.get('/:id', [authMiddleware], errorHandler(getOrderById));
 
 export default orderRoutes
